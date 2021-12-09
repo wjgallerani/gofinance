eae2907b292b0df573948660c823bb70aefe8c77
William Jose Gallerani <wjgallerani@saomanoel.com.br>
2021-12-08 23:32:52 -0300

    Atualização

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/25794349?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>William</UserName>
+
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
+        </Container>
+    );
+}
